5d0edd846f7fc8e5c655766fd9932f80f59ddb90
upobir <red.abiir@gmail.com>
2021-08-06 11:43:06 +0600

    added random_prime_in_range

--- nt_tools/primes.py
+++ nt_tools/primes.py
@@ -1,5 +1,7 @@
-from typing import List
-from math import sqrt, ceil
+from typing import List, Optional
+from math import sqrt, ceil, log
+from random import randint
+from nt_tools.factorization import is_prime
 
 def primes_in_range(L: int, R: int) -> List[int]:
     """generates and returns list of prime in range [L, R)"""
@@ -17,11 +19,11 @@ def primes_in_range(L: int, R: int) -> List[int]:
         for m in range(p*p, limit, p):
             marked1[m] = True
 
-        start: int = max(3, (L+p-1)//p) * p
-        if start % 2 == 0:
-            start += p
+        m_start: int = max(3, (L+p-1)//p) * p
+        if m_start % 2 == 0:
+            m_start += p
 
-        for m in range(start, R, p+p):
+        for m in range(m_start, R, p+p):
             marked2[m-L] = True
 
     start: int = L if L % 2 == 1 else L+1
@@ -31,3 +33,20 @@ def primes_in_range(L: int, R: int) -> List[int]:
         result = [2] + result
 
     return result
+
+def random_prime_in_range(L: int, R: int, max_iter: Optional[int] = None) -> Optional[int]:
+    """generates a random in range [L, R), returns None if max_iter iterations pass but no prime is found"""
+    L = max(L, 1)
+    if L >= R:
+        return None
+
+    if max_iter is None:
+        max_iter = int(log(R)*10)
+
+
+    for _ in range(max_iter):
+        candidate: int = randint(L, R-1)
+        if is_prime(candidate):
+            return candidate
+    
+    return None
